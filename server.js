a6b102291f23fab6c96c98f75a8eeb65a29fe9fa
Alexander Wunschik <alex@wunschik.net>
2017-03-12 11:21:04 +0100

    added transparent cache

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
+var path = require('path');
 var express = require('express');
 var request = require('request');
+var favicon = require('serve-favicon')
 var cheerio = require('cheerio');
 var jsdap = require('jsdap');
 
@@ -8,93 +10,96 @@ app.set('view engine', 'ejs');
 
 var config = require('./server.config.json');
 
-serverIndex = 2;
-
+serverIndex = 0;
 baseUrl = config.servers[serverIndex].base;
 defaultRoute = config.servers[serverIndex].defaultroute;
 baseFtps = config.servers[serverIndex].dataMirrors;
 
+// Favicon Middleware
+app.use(favicon(path.join(__dirname, 'public', 'favicon.png')));
 
+// Logging Middleware
 app.use(function(req, res, next) {
 	console.log(new Date(), req.ip, req.originalUrl);
 	next();
 })
 
 app.get('/', function (req, res) {
+	//TODO show server selection page
 	res.redirect(defaultRoute);
 })
 
 app.get('*.xml', function (req, res) {
 	var path = baseUrl + req.originalUrl;
 		request(path, function (error, response, body) {
-				try {
-			if (error) {
-				res.status(response ? response.statusCode : 404).end(error);
-			}
-			if (!response || response.statusCode != 200) {
-				res.status(response ? response.statusCode : 404).end(response);
-			}
-
-			var xml = cheerio.load(body);
-
-			var title = xml('catalog').attr('name');
-
-			var links = [];
-			var catalogRefs = xml('catalogRef');
-			for (var i=0; i<catalogRefs.length; i++) {
-				var catalogRef = catalogRefs[i];
-				links.push({
-					href: catalogRef.attribs['xlink:href'],
-					title: catalogRef.attribs['xlink:title']
-				});
-			}
-
-			var datasets = [];
-			xml('catalog dataset').each(function() {
-				var baseOdap = xml('catalog service[name="odap"]').attr('base')
-					|| xml('catalog service[serviceType="OPENDAP"]').attr('base') || '/';
-				var baseHttp = baseUrl + (xml('catalog service[name="http"]').attr('base')
-					|| xml('catalog service[serviceType="HTTPServer"]').attr('base') || '/');
-
-				var dataPath = xml(this).attr('urlpath');
-				if (dataPath) {
-					var name = xml(this).attr('name');
-					var id = xml(this).attr('id');
-
-					var dataSize = xml('datasize', this);
-					var size = dataSize.text() + ' ' + dataSize.attr('units');
-
-					var modified = xml('date', this).text();
-
-					var pathMeta = dataPath.startsWith('/') ? dataPath : baseOdap + dataPath;
-					var pathData = dataPath.startsWith('/') ? dataPath : baseHttp + dataPath;
-
-					var ftpPath = (dataPath.startsWith('/') ? dataPath : '/' + dataPath);
-					var pathFtp = baseFtps.map(function(ftp) {
-						return ftp + ftpPath;
-					});
-
-					datasets.push({
-						name: name,
-						size: size,
-						modified: modified,
-						id: id,
-						pathMeta: pathMeta,
-						pathData: pathData,
-						pathFtp: pathFtp
-					});
-
-				} else {
-					title = xml(this).attr('id');
+			try {
+				if (error) {
+					res.status(response ? response.statusCode : 404).end(error);
 				}
-			});
+				if (!response || response.statusCode != 200) {
+					res.status(response ? response.statusCode : 404).end(response);
+				}
 
-			res.render('catalog', {
-				title: title,
-				links: links,
-				datasets: datasets,
-				xml: body
-			});
+				var xml = cheerio.load(body);
+
+				var title = xml('catalog').attr('name');
+
+				var links = [];
+				var catalogRefs = xml('catalogRef');
+				for (var i=0; i<catalogRefs.length; i++) {
+					var catalogRef = catalogRefs[i];
+					links.push({
+						href: catalogRef.attribs['xlink:href'],
+						title: catalogRef.attribs['xlink:title']
+					});
+				}
+
+				var datasets = [];
+				xml('catalog dataset').each(function() {
+					var baseOdap = xml('catalog service[name="odap"]').attr('base')
+						|| xml('catalog service[serviceType="OPENDAP"]').attr('base') || '/';
+					var baseHttp = baseUrl + (xml('catalog service[name="http"]').attr('base')
+						|| xml('catalog service[serviceType="HTTPServer"]').attr('base') || '/');
+
+					var dataPath = xml(this).attr('urlpath');
+					if (dataPath) {
+						var name = xml(this).attr('name');
+						var id = xml(this).attr('id');
+
+						var dataSize = xml('datasize', this);
+						var size = dataSize.text() + ' ' + dataSize.attr('units');
+
+						var modified = xml('date', this).text();
+
+						var pathMeta = dataPath.startsWith('/') ? dataPath : baseOdap + dataPath;
+						var pathData = dataPath.startsWith('/') ? dataPath : baseHttp + dataPath;
+
+						var ftpPath = (dataPath.startsWith('/') ? dataPath : '/' + dataPath);
+						var pathFtp = baseFtps.map(function(ftp) {
+							return ftp + ftpPath;
+						});
+
+						datasets.push({
+							name: name,
+							size: size,
+							modified: modified,
+							id: id,
+							pathMeta: pathMeta,
+							pathData: pathData,
+							pathFtp: pathFtp
+						});
+
+					} else {
+						title = xml(this).attr('id');
+					}
+				});
+
+				res.render('catalog', {
+					title: title,
+					links: links,
+					datasets: datasets,
+					xml: body
+				});
 		} catch (err) {
 			res.status(404);
 			res.render('error', {
@@ -103,7 +108,6 @@ app.get('*.xml', function (req, res) {
 			});
 		}
 	});
-
 })
 
 app.get('*.nc?', function (req, res) {
@@ -129,6 +133,6 @@ app.use(function(req, res, next) {
 	});
 })
 
-app.listen(config.express.port, function () {
-	console.log('listening on port ' + config.express.port);
+app.listen(config.server.port, function () {
+	console.log('listening on port ' + config.server.port);
 })
